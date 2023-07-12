4fb8c0c1cb542a1ec0745768829451d134fda62f
Magda <magdagvirjishvili@gmail.com>
2023-07-12 19:49:05 +0400

    magdas commit

---
+++ numberOfVowels/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main()
+    {
+        int counter = 0;
+        Console.WriteLine("Enter the text - ");
+
+       
+        foreach (char c in Console.ReadLine())
+        {
+          switch (c)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+
+                    counter++;
+                    break;
+            }
+        }
+        Console.WriteLine(counter);
+    }
+
+}
